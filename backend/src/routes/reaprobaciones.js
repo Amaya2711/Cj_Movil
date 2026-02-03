253251fc3c16ee0ad93fe4fcff4d2714f04abeaf
Juan Manuel <juan.manuel.amaya.suarez@gmail.com>
2026-02-02 19:04:50 -0500

    Cj_Prd_v1_6

--- backend/src/routes/reaprobaciones.js
+++ backend/src/routes/reaprobaciones.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { consultarReaprobaciones, aprobarReaprobacion } from '../controllers/reaprobarController1.js';
+import { consultarReaprobaciones, aprobarReaprobacion } from '../controllers/reaprobarController.js';
 
 const router = Router();
 
